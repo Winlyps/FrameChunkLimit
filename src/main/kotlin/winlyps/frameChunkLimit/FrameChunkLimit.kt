a6a542d2158fee6c6a34dec4c7a8340faf6a35b5
Winlypse <nts3@o2.pl>
2025-07-06 21:38:40 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/frameChunkLimit/FrameChunkLimit.kt
@@ -0,0 +1,14 @@
+package winlyps.frameChunkLimit
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FrameChunkLimit : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
